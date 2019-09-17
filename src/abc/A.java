4683b886b8c27437ae646023e4e0774550a988c1
ravina1234 <nikkytoppo93@gmail.com>
2019-09-17 23:13:41 +0530

    First commit

---
+++ src/abc/A.java
@@ -0,0 +1,16 @@
+package abc;
+
+public class A 
+{
+	public void m1()
+	{
+		System.out.println("github......");
+	}
+public static void main(String[] args) {
+	System.out.println("main___A");
+
+A a=new A();
+	a.m1();
+	
+}
+}
